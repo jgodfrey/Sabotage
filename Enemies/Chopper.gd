6b8f4c0400ef765fee4854d0b3ee52c202d1f2bb
Jeff Godfrey <jeff_godfrey@pobox.com>
2020-06-02 19:47:42 -0500

    Chopper death
    - Added particle system
    - Added timer to trigger death after some time
    - Ignored the new "Build" folder

--- Enemies/Chopper.gd
+++ Enemies/Chopper.gd
@@ -3,6 +3,7 @@ extends Area2D
 onready var paratrooper = preload("res://Enemies/Paratrooper.tscn")
 var velocity = Vector2()
 onready var world = get_parent().get_parent()
+var dead = false
 
 func start(pos, dir, speed):
 	position = pos
@@ -15,8 +16,9 @@ func start(pos, dir, speed):
 
 func _process(delta):
 	position += velocity * delta
-	if Utils.getRandomIntBetween(1, 200) == 25:
-		spawnParatrooper()
+	if !dead:
+		if Utils.getRandomIntBetween(1, 200) == 25:
+			spawnParatrooper()
 
 func spawnParatrooper():
 	var p = paratrooper.instance()
@@ -28,5 +30,12 @@ func _on_VisibilityNotifier2D_screen_exited():
 
 func _on_Chopper_area_entered(area):
 	if area.is_in_group("bullet"):
+		dead = true
 		area.queue_free()
-		queue_free()
+		$Particles2D.emitting = true
+		$Sprite.visible = false
+		$CollisionShape2D.set_deferred("disabled", true)
+		$Timer.start()
+
+func _on_Timer_timeout():
+	queue_free()
