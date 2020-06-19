aefd5b1a733287f816f790db77279ff3fccd5e96
Jeff Godfrey <jeff_godfrey@pobox.com>
2020-06-19 13:36:20 -0500

    Fix paratrooper death bug
    - If a paratrooper with a parachute landed on another paratrooper already on the
      ground, the grounded paratrooper was killed. This should only happen in the falling
      paratrooper does not have a parachute (comes in hot). Fixed.
    - Added "falling" boolean to simplify paratrooper state management

--- Enemies/Paratrooper.gd
+++ Enemies/Paratrooper.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var velocity = Vector2()
 var dead = false
+var falling = false
 
 func start(pos, speed):
 	position = pos
@@ -9,32 +10,40 @@ func start(pos, speed):
 
 func _process(delta):
 	if !dead:
-		position += velocity * delta
+		var speed = velocity if not falling else velocity * 2.5
+		position += speed * delta
 
 func _on_Man_area_entered(area):
+	# if we were hit by a bullet, kill the bullet and the paratrooper
 	if area.is_in_group("bullet"):
 		area.queue_free()
 		startDeath()
-		#queue_free()
+	# if we were hit by the ground...
 	elif area.is_in_group("ground"):
 		# if this guy hit the ground without a parachute, kill him
 		if !$Parachute/Sprite.visible:
 			startDeath()
-			#queue_free()
+		# otherwise, hide the parachute and place him on the ground surface
 		else:
 			velocity = Vector2.ZERO
 			position = Vector2(position.x, Globals.screenSize.y - 7)
 			hideParachute()
+	# if we were hit by another paratrooper
 	elif area.is_in_group("man"):
 		var parent = area.get_parent()
-		if !parent.get_node("Parachute/Sprite").visible:
+		# If we're coming in without a parachute and hit another guy on the
+		# ground, kill that guy. The "falling" guy will be killed in a later
+		# frame when he hits the ground without a parachute...
+		if !$Parachute/Sprite.visible && !parent.get_node("Parachute/Sprite").visible:
 			parent.startDeath()
 
 func _on_Parachute_area_entered(area):
+	# If the parachute was hit with a bullet, remove the parachute and
+	# speed up the downward velocity (falling)
 	if area.is_in_group("bullet"):
 		area.queue_free()
 		hideParachute()
-		velocity = velocity * 2.5
+		falling = true
 
 func hideParachute():
 	$Parachute/Sprite.visible = false
